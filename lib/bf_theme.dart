83e4bb101d0238293fd726557c712d8f4382901c
BumBelDumBel <62509344+BumBelDumBel@users.noreply.github.com>
2024-12-05 19:25:54 +0100

    Added BFButton to export.

--- lib/bf_theme.dart
+++ lib/bf_theme.dart
@@ -6,4 +6,5 @@ export 'src/theme/bf_color.dart' show BFColorPacks, BFColors, BFColorPack;
 export 'src/theme/bf_theme.dart' show BFTheme;
 export 'src/theme/theme_extension.dart' show ThemeExtension;
 // Widgets
+export 'src/widgets/bf_button.dart' show BFButton;
 export 'src/widgets/bf_toggle_button.dart' show BFToggleButton;
